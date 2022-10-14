3d1c4f8311b6d8b19f3c30a8df5e51b03d548889
themarshdan <anadaniela.villalba@gmail.com>
2022-10-14 02:19:56 -0300

    primeras primitivas + redimensionar

--- diccionarioImplementacion.go
+++ diccionarioImplementacion.go
@@ -8,6 +8,7 @@ import (
 const (
 	CAPACIDAD_INICIAL = 101
 	FACTOR_DE_CARGA   = 0.8
+	FACTOR_REDIMENSION = 2
 )
 
 type diccionarioImplementacion[K comparable, V any] struct {
@@ -20,9 +21,17 @@ type elementoTabla[K comparable, V any] struct {
 	valor V
 }
 
-func CrearHash[K comparable, V any]() Diccionario[K, V] {
+func crearTabla[K comparable, V any](capacidad int) []TDALista.Lista[elementoTabla[K, V]]{
+	return make([]TDALista.Lista[elementoTabla[K, V]], capacidad)
+	//for i := range dict.tablaValores {
+	//lista := TDALista.CrearListaEnlazada[elementoTabla[K, V]]()
+	//} >>> no se si es necesario
+
+}
+
+func CrearHash[K comparable, V any]() Diccionario[K, V]{
 	dict := new(diccionarioImplementacion[K, V])
-	dict.tablaValores = make([]TDALista.Lista[elementoTabla[K, V]], CAPACIDAD_INICIAL)
+	dict.tablaValores = crearTabla(CAPACIDAD_INICIAL)
 	return dict
 }
 
@@ -32,6 +41,37 @@ func convertirABytes[K comparable](clave K) []byte {
 
 func posicionEnTabla[K comparable](clave K, largo int) int {
 	return funcionHash(convertirABytes(clave)) / largo
+}
+
+func esPrimo(n int) bool {
+	if n <= 1 {
+		return false
+	}
+	for i := 2; i*i <= n; i++ {
+		if n%i == 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func proximoPrimo(n int) int {
+	if esPrimo(n) {
+		return n
+	}
+	return proximoPrimo(n + 1)
+}
+
+func (dict *diccionarioImplementacion[K, V]) redimensionar() {
+	nuevaCapacidad := proximoPrimo(len(dict.tablaValores) * FACTOR_REDIMENSION)
+	nuevaTabla := crearTabla(nuevaCapacidad)
+
+	for iter := dict.Iterador(); iter.HaySiguiente(); {
+		nuevaTabla.InsertarUltimo(iter.VerActual())
+		iter.Siguiente()
+	}
+
+	dict.tablaValores = nuevaTabla
 }
 
 // Guardar guarda el par clave-dato en el Diccionario. Si la clave ya se encontraba, se actualiza el dato asociado
@@ -109,3 +149,11 @@ func (dict *diccionarioImplementacion[K, V]) Borrar(clave K) V {
 func (dict diccionarioImplementacion[K, V]) Cantidad() int {
 	return dict.elementos
 }
+
+
+// Iterar itera internamente el diccionario, aplicando la función pasada por parámetro a todos los elementos del
+// mismo
+Iterar(func(clave K, dato V) bool)
+
+// Iterador devuelve un IterDiccionario para este Diccionario
+Iterador() IterDiccionario[K, V]
